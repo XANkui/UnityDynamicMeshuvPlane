c6e0a817ef4102123fb963c920559a81ffad7f0f
xan <xan@pacificfuture.co>
2019-06-12 15:40:51 +0800

    在Unity中，簡單的動態繪製Mesh平面，簡單處理材質貼圖uv信息，基本實現動態平面繪製

---
+++ Assets/Scripts/MeshPaint.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 动态Mesh绘制
+/// 要求有 MeshFilter MeshRenderer
+/// </summary>
+[RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+public class MeshPaint : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    /// <summary>
+    /// 传点Mesh绘制面
+    /// </summary>
+    /// <param name="points">传入的点列表</param>
+    /// 点的要求：
+    /// 1、这些面的边界点，请顺时针添加进的列表点（由于单面显示，逆时针的话可能面朝下，从上可能会看不见）
+    /// 2、这里的点要求是水平面上的点，因为目前这个函数主要用于水平面上（由于uv处理，目前只做了水平面上的）
+    public void MeshDrawQuad(List<Vector3> points)
+    {
+        Debug.Log("MeshDrawQuad +++++++++++");
+
+        Mesh quadMesh = new Mesh();
+        quadMesh.vertices = points.ToArray();
+        int coutArray = (points.Count - 2);
+        int[] quadPoints = new int[coutArray * 3];
+
+        // 以点队列的第 0 个点为定点，三点绘制一面，三点面的排序
+        for (int i = 0; i < coutArray; i++)
+        {
+
+            quadPoints[i * 3] = 0;
+            quadPoints[(i * 3) + 1] = i + 1;
+            quadPoints[(i * 3) + 2] = i + 2;
+
+        }
+
+        quadMesh.triangles = quadPoints;
+
+        // 水平面的，所以 uv 把 点的 x z 传递给 uv 处理
+        Vector2[] uvs = new Vector2[points.Count];
+        for (int i = 0; i < uvs.Length; i++) {
+            uvs[i] = new Vector2(points[i].x, points[i].z);
+        }
+
+        quadMesh.uv = uvs;
+
+        quadMesh.RecalculateBounds();
+        quadMesh.RecalculateNormals();
+        quadMesh.RecalculateTangents();
+
+        GetComponent<MeshFilter>().mesh = quadMesh;
+    }
+
+
+}
---
+++ Assets/Scripts/TestDynamicPaintMesh.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestDynamicPaintMesh : MonoBehaviour
+{
+    // Mesh 预制体，最好添加材质，因为简单的画 Mesh 做了 uv 处理
+    public GameObject meshPaintPrefab;
+
+    private GameObject meshPaint;
+    private List<Vector3> points;       // 准备传给Mesh绘制的点集
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        points = new List<Vector3>();
+
+        points.Add(new Vector3(-2,0,0));
+        points.Add(new Vector3(0,0,3));
+        points.Add(new Vector3(2,0,0));
+        points.Add(new Vector3(0,0,-2));
+
+        // 生成实体，并添加 MeshPaint脚本
+        meshPaint = GameObject.Instantiate(meshPaintPrefab);
+        meshPaint.AddComponent<MeshPaint>().MeshDrawQuad(points);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 动态更新点集
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            points.Clear();
+            points.Add(new Vector3(-3, 0, 0));
+            
+            points.Add(new Vector3(0, 0, 4));
+            points.Add(new Vector3(3, 0, 4));
+            points.Add(new Vector3(3, 0, 0));
+            points.Add(new Vector3(0, 0, -3));
+
+            // 可以最好添加一个判断，判断是否有 MeshPaint
+            meshPaint.GetComponent<MeshPaint>().MeshDrawQuad(points);
+        }
+    }
+}
